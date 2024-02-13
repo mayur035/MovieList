cdb77c1c64d314dcb1fbfdf020348e41c01123fe
mayur035 <mgp312002@gmail.com>
2024-02-13 15:14:54 +0530

    Using useEffect() For Requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './App.css';
 import Card from './UI/card';
 import Button from './UI/Button';
@@ -7,24 +7,40 @@ import MovieList from './components/movieList';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
 
-  const fetchHandler = async () => {
-    setIsLoading(true);
-    const fetchMovie = await fetch('https://swapi.dev/api/films/');
-    const data = await fetchMovie.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
+  const fetchHandler = useCallback(async () => {
+      setIsLoading(true);
+      setIsError(null);
+
+      try {
+        const fetchMovie = await fetch('https://swapi.dev/api/films/');
+        if (!fetchMovie.ok) {
+          throw new Error('occcchhhhhh!Buddy Kuch issue hai....!')
+        }
+        const data = await fetchMovie.json();
+
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          }
+        })
+        setMovies(transformedMovies);
+      } catch (error) {
+        setIsError(error.message);
       }
-    })
+      setIsLoading(false)
+  }, [])
 
-    setMovies(transformedMovies);
-    setIsLoading(false);
 
-  }
+  useEffect(() => {
+    fetchHandler();
+  }, [fetchHandler])
+
+
   return (
     <React.Fragment>
       <Card>
@@ -32,7 +48,8 @@ function App() {
       </Card>
       <Card>
         {!isLoading && movies.length > 0 && <MovieList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {!isLoading && movies.length === 0 && !isError && <p>Found no movies.</p>}
+        {!isLoading && isError && <p>{isError}</p>}
         {isLoading && <p>Loading....</p>}
 
       </Card>
